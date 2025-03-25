e4a0a7503c4f22f1d147ac038d8415ae7259ed5c
Mohammed-hussain0307 <fairhussain94@gmail.com>
2025-03-25 19:39:52 +0530

    Update userdetail.sql

--- PassportGeneratingSystem/userdetail.sql
+++ PassportGeneratingSystem/userdetail.sql
@@ -1,68 +1,275 @@
-CREATE TABLE user_data(
+use passport;
+
+drop table user_detail
+
+select * from user_detail
+
+ALTER TABLE user_detail ADD message_info VARCHAR(50)
+ALTER TABLE user_detail ADD passport_number VARCHAR(10)
+
+CREATE TABLE user_detail(
 	id INT PRIMARY KEY IDENTITY,
-	given_name VARCHAR(30) NOT NULL,
-	sure_name VARCHAR(30),
-	email_id VARCHAR(30) UNIQUE NOT NULL,
-	login_id VARCHAR(20) UNIQUE NOT NULL,
-	login_password VARCHAR(30) NOT NULL
-)
+    givenName VARCHAR(30) NOT NULL,
+    sureName VARCHAR(30) NOT NULL,
+    gender VARCHAR(10) NOT NULL,
+    dateOfBirth DATE NOT NULL,
+    placeOfBirth VARCHAR(30),
+    maritalStatus VARCHAR(20),
+    employmentType VARCHAR(30),
+    educationQualification VARCHAR(30),
+    aadhaarNumber BIGINT UNIQUE,
+    
+    fatherGivenName VARCHAR(30),
+    fatherSureName VARCHAR(30),
+    motherGivenName VARCHAR(30),
+    motherSureName VARCHAR(30),
+    spousesGivenName VARCHAR(30),
+    spousesSureName VARCHAR(30),
+    
+    houseStreet VARCHAR(255) NOT NULL,
+    villageTownCity VARCHAR(30) NOT NULL,
+    addressState VARCHAR(30) NOT NULL,
+    addressDistrict VARCHAR(30) NOT NULL,
+    policeStation VARCHAR(30) NOT NULL,
+    pincode VARCHAR(10) NOT NULL,
+    mobileNumber BIGINT NOT NULL,
+    emailID VARCHAR(50),
+    
+    contactName VARCHAR(30) NOT NULL,
+    contactMobileNumber BIGINT NOT NULL,
 
-CREATE TABLE admin_data(
-	id INT PRIMARY KEY IDENTITY,
-	admin_name VARCHAR(30) NOT NULL,
-	email_id VARCHAR(30) UNIQUE NOT NULL,
-	admin_id VARCHAR(20) UNIQUE NOT NULL,
-	admin_password VARCHAR(30) NOT NULL
-)
+	status VARCHAR(20),
+	admin_status VARCHAR(20),
+	officer_status VARCHAR(20),
 
-SELECT * FROM admin_data
+	user_id INT,
+	FOREIGN KEY(user_id) REFERENCES user_data(id)
+);
 
-CREATE TABLE police_data(
-	id INT PRIMARY KEY IDENTITY,
-	officer_name VARCHAR(30) NOT NULL,
-	email_id VARCHAR(30) UNIQUE NOT NULL,
-	officer_id VARCHAR(20) UNIQUE NOT NULL,
-	officer_password VARCHAR(30) NOT NULL
-)
+-- ADD USER IN THE TABLE ---
 
-CREATE PROCEDURE SPL_Officer(
-	@OfficerID VARCHAR(20),
-	@Password VARCHAR(20)
+ALTER PROCEDURE SPC_UserDetail(
+    @GivenName VARCHAR(30),
+    @SureName VARCHAR(30),
+    @Gender VARCHAR(10),
+    @DateOfBirth DATE,
+    @PlaceOfBirth VARCHAR(30) = NULL,
+    @MaritalStatus VARCHAR(20),
+    @EmploymentType VARCHAR(30) = NULL,
+    @EducationQualification VARCHAR(30) = NULL,
+    @AadhaarNumber BIGINT = NULL,
+    
+    @FatherGivenName VARCHAR(30),
+    @FatherSureName VARCHAR(30) = NULL,
+    @MotherGivenName VARCHAR(30),
+    @MotherSureName VARCHAR(30) = NULL,
+    @SpousesGivenName VARCHAR(30) = NULL,
+    @SpousesSureName VARCHAR(30) = NULL,
+    
+    @HouseStreet VARCHAR(255),
+    @VillageTownCity VARCHAR(30),
+    @AddressState VARCHAR(30),
+    @AddressDistrict VARCHAR(30),
+    @PoliceStation VARCHAR(30),
+    @Pincode VARCHAR(10),
+    @MobileNumber BIGINT,
+    @EmailID VARCHAR(50) = NULL,
+    
+    @ContactName VARCHAR(30),
+    @ContactMobileNumber BIGINT,
+
+	@UserID INT
 )
 AS
 BEGIN
-	SELECT * FROM police_data WHERE officer_id = @OfficerID AND officer_password = @Password
+	INSERT INTO user_detail(givenName,sureName,gender,dateOfBirth,placeOfBirth,maritalStatus,employmentType,educationQualification,aadhaarNumber,
+							fatherGivenName,fatherSureName,motherGivenName,motherSureName,spousesGivenName,spousesSureName,
+							houseStreet,villageTownCity,addressState,addressDistrict,policeStation,pincode,mobileNumber,emailID,
+							contactName,contactMobileNumber,user_id)
+						VALUES(@GivenName,@SureName,@Gender,@DateOfBirth,@PlaceOfBirth,@MaritalStatus,@EmploymentType,@EducationQualification,@AadhaarNumber,
+							@FatherGivenName,@FatherSureName,@MotherGivenName,@MotherSureName,@SpousesGivenName,@SpousesSureName,
+							@HouseStreet,@VillageTownCity,@AddressState,@AddressDistrict,@PoliceStation,@Pincode,@MobileNumber,@EmailID,
+							@ContactName,@ContactMobileNumber,@UserID)
 END;
 
-CREATE PROCEDURE SPL_Admin(
-	@AdminID VARCHAR(20),
-	@Password VARCHAR(20)
-)
+-- READ APPLICATION BASED ON LOGIN USER FROM THE TABLE --
+
+ALTER PROCEDURE SPR_AllUser @UserID INT
 AS
 BEGIN
-	SELECT * FROM admin_data WHERE admin_id = @AdminID AND admin_password = @Password
+	SELECT * FROM user_detail
+	WHERE user_id = @UserID AND status IS NULL
 END;
 
-ALTER TABLE user_data DROP COLUMN admin_id,admin_password
+EXEC SPR_AllUser @UserID = 2
 
-ALTER PROCEDURE SPC_User(
+-- GET THE SINGLE USER BY ID -- 
+
+CREATE PROCEDURE SPG_User 
+	@ID INT
+AS
+BEGIN
+	SELECT * FROM user_detail WHERE id = @ID
+END;
+
+EXEC SPG_User @ID = 6
+
+-- UPDATE USER DETAILS --
+
+ALTER PROCEDURE SPU_User(
+	@ID INT,
 	@GivenName VARCHAR(30),
-	@SureName VARCHAR(30) = NULL,
-	@EmailID VARCHAR(30),
-	@LoginID VARCHAR(20),
-	@LoginPassword VARCHAR(30)
+    @SureName VARCHAR(30),
+    @Gender VARCHAR(10),
+    @DateOfBirth DATE,
+    @PlaceOfBirth VARCHAR(30) = NULL,
+    @MaritalStatus VARCHAR(20),
+    @EmploymentType VARCHAR(30) = NULL,
+    @EducationQualification VARCHAR(30) = NULL,
+    @AadhaarNumber BIGINT = NULL,
+    
+    @FatherGivenName VARCHAR(30),
+    @FatherSureName VARCHAR(30) = NULL,
+    @MotherGivenName VARCHAR(30),
+    @MotherSureName VARCHAR(30) = NULL,
+    @SpousesGivenName VARCHAR(30) = NULL,
+    @SpousesSureName VARCHAR(30) = NULL,
+    
+    @HouseStreet VARCHAR(255),
+    @VillageTownCity VARCHAR(30),
+    @AddressState VARCHAR(30),
+    @AddressDistrict VARCHAR(30),
+    @PoliceStation VARCHAR(30),
+    @Pincode VARCHAR(10),
+    @MobileNumber BIGINT,
+    @EmailID VARCHAR(50) = NULL,
+    
+    @ContactName VARCHAR(30),
+    @ContactMobileNumber BIGINT
 )
 AS
 BEGIN
-	INSERT INTO user_data(given_name,sure_name,email_id,login_id,login_password) 
-			VALUES(@GivenName,@SureName,@EmailID,@LoginID,@LoginPassword)
+	UPDATE user_detail 
+	SET givenName = @GivenName,
+		sureName = @SureName,
+		gender = @Gender,
+		dateOfBirth = @DateOfBirth,
+		placeOfBirth = @PlaceOfBirth,
+		maritalStatus = @MaritalStatus,
+		employmentType = @EmploymentType,
+		educationQualification = @EducationQualification,
+		aadhaarNumber = @AadhaarNumber,    
+		fatherGivenName = @FatherGivenName,
+		fatherSureName = @FatherSureName,
+		motherGivenName = @MotherGivenName,
+		motherSureName = @MotherSureName,
+		spousesGivenName = @SpousesGivenName,
+		spousesSureName = @SpousesSureName,
+		houseStreet = @HouseStreet,
+		villageTownCity = @VillageTownCity,
+		addressState = @AddressState,
+		addressDistrict = @AddressDistrict,
+		policeStation = @PoliceStation,
+		pincode = @Pincode,
+		mobileNumber = @MobileNumber,
+		emailID = @EmailID,    
+		contactName = @ContactName,
+		contactMobileNumber = @ContactMobileNumber
+		WHERE id = @ID
 END;
 
-ALTER PROCEDURE SPL_User(
-	@LoginID VARCHAR(20),
-	@LoginPassword VARCHAR(30)
+-- DELETE USER DETAIL FROM THE TABLE --
+
+CREATE PROCEDURE SPD_User @ID INT
+AS
+BEGIN
+	DELETE FROM user_detail WHERE id = @ID
+END;
+
+ALTER PROCEDURE SPB_User(
+	@ID INT,
+	@Status VARCHAR(20)
 )
 AS
 BEGIN
-	SELECT * FROM user_data WHERE login_id = @LoginID AND login_password = @LoginPassword
+	UPDATE user_detail 
+	SET status = @Status
+	WHERE id = @ID
+END;
+
+-- READ ALL USER SHOW IN ADMIN PAGE BASED ON USER STATUS -- 
+
+ALTER PROCEDURE SPA_User
+AS
+BEGIN
+	SELECT * FROM user_detail 
+	WHERE status = 'submit' AND (admin_status = 'NULL' OR admin_status IS NULL)
+END;
+
+EXEC SPA_User
+
+-- UPDATE ADMIN STATUS AFTER APPROVED THE USER FORM --
+
+ALTER PROCEDURE SPAP_admin 
+	@ID INT,
+	@MessageInfo VARCHAR(50)
+AS
+BEGIN
+	UPDATE user_detail 
+	SET admin_status = 'approve', message_info = @MessageInfo 
+	WHERE id = @ID
+END;
+
+-- UPDATE ADMIN STATUS AFTER REJECTED THE USER FORM --
+
+ALTER PROCEDURE SPRE_Admin @ID INT
+AS
+BEGIN
+	UPDATE user_detail 
+	SET admin_status = 'reject' 
+	WHERE id = @ID
+END;
+
+-- READ THE APPLICATION BASED ON THE ADMIN STATUS --
+
+ALTER PROCEDURE SPA_Officer
+AS
+BEGIN
+	SELECT * FROM user_detail
+	WHERE admin_status = 'approve' AND officer_status IS NULL
+END;
+
+EXEC SPA_Officer
+
+-- UPDATE OFFICER STATUS IN THE TABLE --
+
+ALTER PROCEDURE SPV_Officer 
+	@ID INT,
+	@AdminStatus VARCHAR(20),
+	@MessageInfo VARCHAR(50)
+AS
+BEGIN
+	UPDATE user_detail 
+	SET officer_status = 'verified',admin_status = @AdminStatus,message_info = @MessageInfo
+	WHERE id = @ID
+END;
+
+ALTER PROCEDURE SPR_Officer 
+	@ID INT,
+	@MessageInfo VARCHAR(50)
+AS
+BEGIN
+	UPDATE user_detail 
+	SET officer_status = 'reject',message_info = @MessageInfo 
+	WHERE id = @ID
+END;
+
+-- GENERATE PASSPORT NUMBER FROM ADMIN PAGE--
+
+ALTER PROCEDURE SPP_Admin 
+	@ID INT,
+	@PassportNumber VARCHAR(10)
+AS
+BEGIN
+	UPDATE user_detail SET passport_number = @PassportNumber WHERE id = @ID
 END;
